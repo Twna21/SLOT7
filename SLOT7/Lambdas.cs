155023d4082cb375f7fbe5ae1945c7f7ab425a0c
Tuong <tuongnmde170578@fpt.edu.vn>
2023-09-26 11:31:21 +0700

    Add project files.

---
+++ SLOT7/Lambdas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLOT7
+{
+    internal class Lambdas
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string[] names = { "david", "jane", "peter", "jone", "mark" };
+
+                foreach (string item in names.OrderBy(s => s)) { Console.WriteLine(item); }
+
+            }
+        }
+    }
+}
